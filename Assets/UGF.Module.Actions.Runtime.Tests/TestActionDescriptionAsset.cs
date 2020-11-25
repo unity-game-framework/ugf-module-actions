b284f194d10c2052880e0df595123f2b727e46d9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-25 23:55:50 +0300

    Add tests

---
+++ Assets/UGF.Module.Actions.Runtime.Tests/TestActionDescriptionAsset.cs
@@ -0,0 +1,33 @@
+﻿using UGF.Actions.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Actions.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestActionDescriptionAsset")]
+    public class TestActionDescriptionAsset : ActionDescriptionAssetBase
+    {
+        protected override IActionDescription OnBuild()
+        {
+            return new TestActionDescription();
+        }
+    }
+
+    public class TestAction : ActionDescribed<TestActionDescription>
+    {
+        public TestAction(TestActionDescription description) : base(description)
+        {
+        }
+
+        protected override void OnExecute(IActionProvider provider, IActionContext context)
+        {
+        }
+    }
+
+    public class TestActionDescription : ActionDescriptionBase
+    {
+        protected override IAction OnBuild()
+        {
+            return new TestAction(this);
+        }
+    }
+}
