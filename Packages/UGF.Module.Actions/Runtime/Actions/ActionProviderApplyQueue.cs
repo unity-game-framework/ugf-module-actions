090ff878241a8a7fb375a3bc1ca19add14bc8429
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-26 00:07:56 +0300

    Add ActionProviderApplyQueue

---
+++ Packages/UGF.Module.Actions/Runtime/Actions/ActionProviderApplyQueue.cs
@@ -0,0 +1,16 @@
+﻿using UGF.Actions.Runtime;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Actions.Runtime.Actions
+{
+    public class ActionProviderApplyQueue : ActionBase
+    {
+        protected override void OnExecute(IActionProvider provider, IActionContext context)
+        {
+            var application = context.Get<IApplication>();
+            var module = application.GetModule<IActionModule>();
+
+            module.Provider.ApplyQueued();
+        }
+    }
+}
---
+++ Packages/UGF.Module.Actions/Runtime/Actions/ActionProviderApplyQueueAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.Actions.Runtime.Actions
+{
+    [CreateAssetMenu(menuName = "UGF/Actions/Actions/Action Provider Apply Queue", order = 2000)]
+    public class ActionProviderApplyQueueAsset : ActionDescriptionAsset<ActionProviderApplyQueue>
+    {
+    }
+}
