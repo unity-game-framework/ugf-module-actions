9ddc560590794533eb5dddb2be47d49bad008199
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-27 22:05:58 +0300

    Add tests

--- Assets/UGF.Module.Actions.Runtime.Tests/TestActionModule.cs
+++ Assets/UGF.Module.Actions.Runtime.Tests/TestActionModule.cs
@@ -1,6 +1,38 @@
+using NUnit.Framework;
+using UGF.Application.Runtime;
+using UnityEngine;
+
 namespace UGF.Module.Actions.Runtime.Tests
 {
     public class TestActionModule
     {
+        [Test]
+        public void InitializeAndUninitialize()
+        {
+            IApplication application = CreateApplication();
+
+            application.Initialize();
+
+            var module = application.GetModule<IActionModule>();
+
+            Assert.NotNull(module);
+
+            application.Uninitialize();
+        }
+
+        private IApplication CreateApplication()
+        {
+            return new ApplicationConfigured(new ApplicationResources
+            {
+                new ApplicationConfig
+                {
+                    Modules =
+                    {
+                        (IApplicationModuleAsset)Resources.Load("UpdateModule", typeof(IApplicationModuleAsset)),
+                        (IApplicationModuleAsset)Resources.Load("ActionModule", typeof(IApplicationModuleAsset))
+                    }
+                }
+            });
+        }
     }
 }
