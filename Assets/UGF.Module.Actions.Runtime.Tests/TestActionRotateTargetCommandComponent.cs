67d943a4bcdab55a78176bd4d550ad0d4f984fa8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-28 22:42:38 +0300

    Add test scene

---
+++ Assets/UGF.Module.Actions.Runtime.Tests/TestActionRotateTargetCommandComponent.cs
@@ -0,0 +1,27 @@
+﻿using UGF.Application.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Actions.Runtime.Tests
+{
+    public class TestActionRotateTargetCommandComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform m_transform;
+        [SerializeField] private Vector3 m_rotation;
+
+        public Transform Transform { get { return m_transform; } set { m_transform = value; } }
+        public Vector3 Rotation { get { return m_rotation; } set { m_rotation = value; } }
+
+        private void Update()
+        {
+            if (ApplicationInstance.HasApplication)
+            {
+                var module = ApplicationInstance.Application.GetModule<IActionModule>();
+
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    module.Provider.Add(new TestActionRotateTargetCommand(m_transform, m_rotation));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/UGF.Module.Actions.Runtime.Tests/TestActionRotateTargetDescriptionAsset.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UGF.Actions.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Actions.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestActionRotateTargetDescriptionAsset")]
+    public class TestActionRotateTargetDescriptionAsset : ActionDescriptionAssetBase
+    {
+        [SerializeField] private float m_speed = 1F;
+
+        public float Speed { get { return m_speed; } set { m_speed = value; } }
+
+        protected override IActionDescription OnBuild()
+        {
+            return new TestActionRotateTargetDescription(m_speed);
+        }
+    }
+
+    public class TestActionRotateTargetDescription : ActionDescriptionBase
+    {
+        public float Speed { get; }
+
+        public TestActionRotateTargetDescription(float speed)
+        {
+            Speed = speed;
+        }
+
+        protected override IAction OnBuild()
+        {
+            return new TestActionRotateTarget(this);
+        }
+    }
+
+    public class TestActionRotateTarget : ActionDescribed<TestActionRotateTargetDescription, TestActionRotateTargetCommand>
+    {
+        public TestActionRotateTarget(TestActionRotateTargetDescription description) : base(description)
+        {
+        }
+
+        protected override void OnExecute(IActionProvider provider, IActionContext context, TestActionRotateTargetCommand command)
+        {
+            command.Transform.Rotate(command.Rotation * Description.Speed * Time.deltaTime);
+        }
+    }
+
+    public readonly struct TestActionRotateTargetCommand : IActionCommand
+    {
+        public Transform Transform { get; }
+        public Vector3 Rotation { get; }
+
+        public TestActionRotateTargetCommand(Transform transform, Vector3 rotation)
+        {
+            Transform = transform ? transform : throw new ArgumentNullException(nameof(transform));
+            Rotation = rotation;
+        }
+    }
+}
