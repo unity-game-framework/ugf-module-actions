5a4da582ec52053d15bebf3c9493486e6c1b7044
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-18 18:17:18 +0300

    Change IActionModuleDescription

--- Packages/UGF.Module.Actions/Runtime/ActionModuleDescription.cs
+++ Packages/UGF.Module.Actions/Runtime/ActionModuleDescription.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections.Generic;
-using UGF.Actions.Runtime;
-using UGF.Application.Runtime;
+﻿using UGF.Application.Runtime;
 
 namespace UGF.Module.Actions.Runtime
 {
     public class ActionModuleDescription : ApplicationModuleDescription, IActionModuleDescription
     {
-        public Dictionary<string, IActionSystemBuilder> Systems { get; } = new Dictionary<string, IActionSystemBuilder>();
-
-        IReadOnlyDictionary<string, IActionSystemBuilder> IActionModuleDescription.Systems { get { return Systems; } }
     }
 }
--- Packages/UGF.Module.Actions/Runtime/IActionModuleDescription.cs
+++ Packages/UGF.Module.Actions/Runtime/IActionModuleDescription.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using UGF.Actions.Runtime;
-using UGF.Application.Runtime;
+﻿using UGF.Application.Runtime;
 
 namespace UGF.Module.Actions.Runtime
 {
     public interface IActionModuleDescription : IApplicationModuleDescription
     {
-        IReadOnlyDictionary<string, IActionSystemBuilder> Systems { get; }
     }
 }
