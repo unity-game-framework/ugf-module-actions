3c955ecbdbd93d537a5c8278347b4870d8600733
Vorobey <vorobyovvorobyov@gmail.com>
2022-10-13 16:11:51 +0200

    Add ActionSystemDescribed

---
+++ Packages/UGF.Module.Actions/Runtime/ActionSystemDescribed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UGF.Actions.Runtime;
+using UGF.Application.Runtime;
+using UGF.Description.Runtime;
+
+namespace UGF.Module.Actions.Runtime
+{
+    public abstract class ActionSystemDescribed<TDescription> : ActionSystem, IDescribed<TDescription> where TDescription : class, IDescription
+    {
+        public TDescription Description { get; }
+        public IApplication Application { get; }
+
+        protected ActionSystemDescribed(TDescription description, IApplication application)
+        {
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Application = application ?? throw new ArgumentNullException(nameof(application));
+        }
+
+        public T GetDescription<T>() where T : class, IDescription
+        {
+            return (T)GetDescription();
+        }
+
+        public IDescription GetDescription()
+        {
+            return Description;
+        }
+    }
+}
---
+++ Packages/UGF.Module.Actions/Runtime/ActionSystemDescribedAsset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UGF.Actions.Runtime;
+using UGF.Application.Runtime;
+using UGF.Builder.Runtime;
+using UGF.Description.Runtime;
+
+namespace UGF.Module.Actions.Runtime
+{
+    public abstract class ActionSystemDescribedAsset<TSystem, TDescription> : ActionSystemAsset, IDescribedBuilder<IApplication>, IDescriptionBuilder
+        where TSystem : class, IActionSystem
+        where TDescription : class, IDescription
+    {
+        protected override IActionSystem OnBuild(IApplication arguments)
+        {
+            TDescription description = OnBuildDescription();
+
+            if (description == null) throw new ArgumentNullException(nameof(description), "Description can not be null.");
+
+            return OnBuild(description, arguments);
+        }
+
+        protected abstract TDescription OnBuildDescription();
+        protected abstract TSystem OnBuild(TDescription description, IApplication application);
+
+        T IBuilder<IApplication, IDescribed>.Build<T>(IApplication arguments)
+        {
+            return (T)OnBuild(arguments);
+        }
+
+        IDescribed IBuilder<IApplication, IDescribed>.Build(IApplication arguments)
+        {
+            return (IDescribed)OnBuild(arguments);
+        }
+
+        T IBuilder<IDescription>.Build<T>()
+        {
+            return (T)(object)OnBuildDescription();
+        }
+
+        IDescription IBuilder<IDescription>.Build()
+        {
+            return OnBuildDescription();
+        }
+    }
+}
