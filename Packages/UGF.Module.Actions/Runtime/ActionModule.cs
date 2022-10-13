259f14b74a4d45a821bd50e6c514000a59e3aadb
Vorobey <vorobyovvorobyov@gmail.com>
2022-10-13 16:44:43 +0200

    Change ActionModule

--- Packages/UGF.Module.Actions/Runtime/ActionModule.cs
+++ Packages/UGF.Module.Actions/Runtime/ActionModule.cs
@@ -1,25 +1,27 @@
 using System;
+using System.Threading.Tasks;
 using UGF.Actions.Runtime;
 using UGF.Application.Runtime;
-using UGF.Module.Actions.Runtime.Provider;
+using UGF.Builder.Runtime;
+using UGF.EditorTools.Runtime.Ids;
+using UGF.Initialize.Runtime;
 using UGF.Module.Update.Runtime;
 using UGF.RuntimeTools.Runtime.Contexts;
-using UGF.Update.Runtime;
+using UGF.RuntimeTools.Runtime.Providers;
 
 namespace UGF.Module.Actions.Runtime
 {
-    public class ActionModule : ApplicationModule<ActionModuleDescription>, IActionModule
+    public class ActionModule : ApplicationModuleAsync<ActionModuleDescription>, IActionModule
     {
+        public IProvider<GlobalId, IActionSystem> Systems { get; } = new Provider<GlobalId, IActionSystem>();
         public IActionProvider Provider { get; }
         public IContext Context { get; }
-        public IUpdateGroup ProviderApplyQueueUpdateGroup { get { return m_providerApplyQueueUpdateGroup ?? throw new ArgumentException("Provider apply queue update group not created."); } }
-        public bool HasProviderApplyQueueUpdateGroup { get { return m_providerApplyQueueUpdateGroup != null; } }
 
         protected IUpdateModule UpdateModule { get; }
 
         IActionModuleDescription IActionModule.Description { get { return Description; } }
 
-        private readonly IUpdateGroup m_providerApplyQueueUpdateGroup;
+        private readonly InitializeCollection<IActionSystem> m_systemInitialize = new InitializeCollection<IActionSystem>();
 
         public ActionModule(ActionModuleDescription description, IApplication application) : this(description, application, new ActionProvider(), new Context { application })
         {
@@ -31,36 +33,52 @@ namespace UGF.Module.Actions.Runtime
             Context = context ?? throw new ArgumentNullException(nameof(context));
 
             UpdateModule = Application.GetModule<IUpdateModule>();
-
-            if (Description.ProviderApplyQueueUpdateGroupCreate)
-            {
-                m_providerApplyQueueUpdateGroup = new UpdateGroup<IUpdateHandler>(new UpdateList<IUpdateHandler>());
-
-                m_providerApplyQueueUpdateGroup.Collection.Add(new ActionSystemUpdatable(Provider, Context)
-                {
-                    new ProviderApplyQueueAction(Application)
-                });
-            }
         }
 
         protected override void OnInitialize()
         {
             base.OnInitialize();
 
-            if (HasProviderApplyQueueUpdateGroup)
+            foreach ((GlobalId key, IBuilder<IApplication, IActionSystem> value) in Description.Systems)
             {
-                UpdateModule.Provider.AddWithSubSystemType(m_providerApplyQueueUpdateGroup, Description.ProviderApplyQueueUpdateGroupTargetSystemType);
+                IActionSystem system = value.Build(Application);
+
+                Systems.Add(key, system);
+
+                m_systemInitialize.Add(system);
             }
+
+            m_systemInitialize.Initialize();
+        }
+
+        protected override async Task OnInitializeAsync()
+        {
+            await m_systemInitialize.InitializeAsync();
         }
 
         protected override void OnUninitialize()
         {
             base.OnUninitialize();
 
-            if (HasProviderApplyQueueUpdateGroup)
-            {
-                UpdateModule.Provider.Remove(m_providerApplyQueueUpdateGroup);
-            }
+            m_systemInitialize.Uninitialize();
+            m_systemInitialize.Clear();
+
+            Systems.Clear();
+        }
+
+        public void ExecuteSystem(GlobalId id)
+        {
+            ExecuteSystem(id, Context);
+        }
+
+        public void ExecuteSystem(GlobalId id, IContext context)
+        {
+            if (!id.IsValid()) throw new ArgumentException("Value should be valid.", nameof(id));
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            IActionSystem system = Systems.Get(id);
+
+            system.Execute(Provider, context);
         }
     }
 }
--- Packages/UGF.Module.Actions/Runtime/ActionModuleAsset.cs
+++ Packages/UGF.Module.Actions/Runtime/ActionModuleAsset.cs
@@ -1,6 +1,6 @@
-﻿using UGF.Application.Runtime;
-using UGF.EditorTools.Runtime.IMGUI.Types;
-using UGF.Module.Update.Runtime;
+﻿using System.Collections.Generic;
+using UGF.Application.Runtime;
+using UGF.EditorTools.Runtime.Assets;
 using UnityEngine;
 
 namespace UGF.Module.Actions.Runtime
@@ -8,21 +8,23 @@ namespace UGF.Module.Actions.Runtime
     [CreateAssetMenu(menuName = "Unity Game Framework/Actions/Action Module", order = 2000)]
     public class ActionModuleAsset : ApplicationModuleAsset<IActionModule, ActionModuleDescription>
     {
-        [SerializeField] private bool m_providerApplyQueueUpdateGroupCreate = true;
-        [UpdateSystemTypeDropdown]
-        [SerializeField] private TypeReference<object> m_providerApplyQueueUpdateGroupTargetSystemType;
+        [SerializeField] private List<AssetIdReference<ActionSystemAsset>> m_systems = new List<AssetIdReference<ActionSystemAsset>>();
 
-        public bool ProviderApplyQueueUpdateGroupCreate { get { return m_providerApplyQueueUpdateGroupCreate; } set { m_providerApplyQueueUpdateGroupCreate = value; } }
-        public TypeReference<object> ProviderApplyQueueUpdateGroupTargetSystemType { get { return m_providerApplyQueueUpdateGroupTargetSystemType; } set { m_providerApplyQueueUpdateGroupTargetSystemType = value; } }
+        public List<AssetIdReference<ActionSystemAsset>> Systems { get { return m_systems; } }
 
         protected override IApplicationModuleDescription OnBuildDescription()
         {
             var description = new ActionModuleDescription
             {
-                RegisterType = typeof(IActionModule),
-                ProviderApplyQueueUpdateGroupCreate = m_providerApplyQueueUpdateGroupCreate,
-                ProviderApplyQueueUpdateGroupTargetSystemType = m_providerApplyQueueUpdateGroupTargetSystemType.Get()
+                RegisterType = typeof(IActionModule)
             };
+
+            for (int i = 0; i < m_systems.Count; i++)
+            {
+                AssetIdReference<ActionSystemAsset> reference = m_systems[i];
+
+                description.Systems.Add(reference.Guid, reference.Asset);
+            }
 
             return description;
         }
