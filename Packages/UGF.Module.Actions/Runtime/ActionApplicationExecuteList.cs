895692e043c3c3a0cccabf96f7cd64eb74241f20
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-08 16:25:10 +0100

    Add ActionApplicationExecuteList

---
+++ Packages/UGF.Module.Actions/Runtime/ActionApplicationExecuteList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UGF.Actions.Runtime;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Actions.Runtime
+{
+    public abstract class ActionApplicationExecuteList : ActionExecuteList
+    {
+        public IApplication Application { get; }
+
+        protected ActionApplicationExecuteList(IApplication application)
+        {
+            Application = application ?? throw new ArgumentNullException(nameof(application));
+        }
+    }
+}
